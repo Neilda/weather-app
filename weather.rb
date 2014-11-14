7c51f57f2624c37bf64be5bcc27074c2a831d65e
Neilda <neilda.pacquing@gmail.com>
2014-11-14 00:23:01 +0000

    CSS style and images loaded

--- weather.rb
+++ weather.rb
@@ -2,30 +2,39 @@ require 'sinatra'
 require 'yahoo_weatherman'
 
 def get_weather(location)
-	client = Weatherman::Client.new
-	client.lookup_by_location(location).condition['text']
+    client = Weatherman::Client.new
+    client.lookup_by_location(location).condition['text']
 end
 
 def get_temp(location)
-	client = Weatherman::Client.new
-	client.lookup_by_location(location).condition['temp']
+    client = Weatherman::Client.new
+    client.lookup_by_location(location).condition['temp']
 end
 
 get '/' do
-	erb :home
+    erb :home
 end
-
 post '/weather' do
-    post = params[:post]['location']
+    @post = params[:post]['location']
+    @weather = get_weather(@post).downcase
+    @temp = ((get_temp(@post) * 9)/5) + 32
 
-    @weather = get_weather(post)
- 
     "#{@weather}"
-    
-    if (@weather == 'sunny')
+
+    if(@weather == 'sunny')
+        erb :sunny
+    elsif(@weather == 'fair')
         erb :sunny
     elsif (@weather == 'cloudy')
         erb :cloudy
+    elsif (@weather == 'mostly cloudy')
+        erb :cloudy
+    elsif (@weather == 'partly cloudy')
+        erb :cloudy
+    elsif (@weather == 'rain')
+        erb :rainy
+    elsif (@weather == 'light rain with thunder')
+        erb :rainy
     elsif (@weather == 'snowy')
         erb :snowy
     else
